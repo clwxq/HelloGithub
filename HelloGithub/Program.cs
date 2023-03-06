107e4825f810da73c27f4324bca1d31a0af56a3a
patrik.cermak <patrik.cermak@VT1-08>
2023-03-06 10:42:01 +0100

    Přidejte soubory projektu.

---
+++ HelloGithub/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.ForegroundColor = ConsoleColor.DarkCyan;
+
+string[] zajmy = {"hry obzvlášť MOBA", "jízda na kole"};
+
+Console.WriteLine("Cs cs jsem Patrik, chodím na SŠ Educhem a mám rád/baví mě: ");
+foreach(var item in zajmy)
+{
+    Console.WriteLine(item);
+}
